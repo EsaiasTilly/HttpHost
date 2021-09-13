38c69d797f871ee5417582cb65228f35294a5b7e
Esaias Tilly <esaias@tillys.se>
2021-09-13 17:15:23 +0200

    Serve user content

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ const path = require('path');
 const app = express();
 const port = 5454;
 
+// Serve Landing Page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/root/index.html'));
 });
 
+// Serve User Content
+app.get('/c/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/user_content', req.path.substr(3)));
+});
+
+// Start Listening
 app.listen(port, () => {
     console.log('HttpHost is successfully working and listens at port ' + port);
 });
