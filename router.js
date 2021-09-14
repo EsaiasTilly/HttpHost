cea6819013f04026604b19ba520a0968b48d55e6
Esaias Tilly <esaias@tillys.se>
2021-09-14 14:39:11 +0200

    Fixed uncompleted folders

--- router.js
+++ router.js
@@ -21,20 +21,34 @@ exports.handleLandingPageRoute = handleLandingPageRoute;
  */
 const handleUserContentRoute = (req, res) => {
     try {
-        const reqPath = decodeURI(req.path.substr(2));
-        let absPath = path.join(__dirname, '/user_content/', reqPath);
-        const fsStat = fs.existsSync(absPath) ? fs.statSync(absPath) : false;
-        if(fsStat && fsStat.isDirectory && fs.existsSync(path.join(absPath, 'index.html')))
-        { absPath = path.join(absPath, 'index.html'); }
+        const reqPath = decodeURI(req.path);
+        const reqAbs = path.join(__dirname, '/user_content/', reqPath.substr(2));
+        const reqFs = fs.existsSync(reqAbs) ? fs.statSync(reqAbs) : false;
 
-        if(!fs.existsSync(absPath) || !fs.statSync(absPath).isFile) {
-            res.send('404 - File not found');
-        } else {
-            res.sendFile(absPath);
+        // Provide File If Found
+        if(reqFs && reqFs.isFile()) { res.sendFile(reqAbs); return; }
+
+        // Check If An Uncompleted Directory Url Was Requested
+        if(reqFs && reqFs.isDirectory() && reqPath.lastIndexOf('/') < reqPath.length - 1)
+        { res.redirect(reqPath + '/'); return; }
+
+        // Search For Default Documents
+        if(reqFs && reqFs.isDirectory()) {
+            const defaultDocuments = ['index.html', 'index.htm'];
+            for(let i = 0; i < defaultDocuments.length; i++) {
+                const docName = defaultDocuments[i];
+                const docAbs = path.join(reqAbs, docName);
+                const docFs = fs.existsSync(docAbs) ? fs.statSync(docAbs) : false;
+                if(docFs && docFs.isFile()) { res.sendFile(docAbs); return; }
+                console.log(docName, docFs && docFs.isFile());
+            }
         }
+
+        // If No File Was Found
+        res.sendStatus(404);
     } catch (error) {
         console.error(error);
-        res.send('502 - Internal server error!');
+        res.sendStatus(502);
     }
 }
 
